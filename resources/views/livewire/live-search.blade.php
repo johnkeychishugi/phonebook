60fe4f6aada78ce7a1eea9f17fe98a91fc839679
John Chishugi <jkchishugi@gmail.com>
2020-10-28 22:02:26 +0200

    ft-create-blade-livewire

--- resources/views/livewire/live-search.blade.php
+++ resources/views/livewire/live-search.blade.php
@@ -7,4 +7,16 @@
    			<span class="input-group-text" id="basic-addon2">Search</span>
    		</div>
    </div>
+   <div class="list-group mt-2">
+   		@foreach ($contacts as $contact)
+   			<div class="list-group-item list-group-item-action">
+   				<div class="d-flex w-100 justify-content-between">
+   					<h5 class="mb-1">{{ $contact['name'] }}</h5>
+   					<small class="text-muted">{{ $contact['email'] }}</small>
+   				</div>
+   				<p class="mb-1">{{ $contact['city'] }}</p>
+   				<small class="text-muted">{{ $contact['phone'] }}</small>
+   			</div>
+   		@endforeach
+   </div>
 </div>
