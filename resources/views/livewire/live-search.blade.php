60e5f3874af2be9c50222f746e08c1a8d80563f2
John Chishugi <jkchishugi@gmail.com>
2020-10-28 22:53:29 +0200

    ft-finalization

--- resources/views/livewire/live-search.blade.php
+++ resources/views/livewire/live-search.blade.php
@@ -1,13 +1,24 @@
 <div>
     <h1>Find a Contact</h1>
-    {{ $name }}
-    <div class="input-group mb-3">
-   		<input type="text" wire:model="name" class="form-control" placeholder="Find a contact" id="" aria-label="Find a contact" aria-describedby="basic-addon2" autocomplete="off">
+
+    @if (session()->has('message'))
+    	<div class="alert alert-info">
+    		{{ session('message') }}
+    	</div>
+    @endif
+    <form wire:submit.prevent="searchByName">
+      	<div class="input-group mb-3">
+   			<input type="text" wire:model="name" class="form-control" placeholder="Find a contact" id="" aria-label="Find a contact" aria-describedby="basic-addon2" autocomplete="off">
    		<div class="input-group-append">
    			<span class="input-group-text" id="basic-addon2">Search</span>
    		</div>
-   </div>
-   <div class="list-group mt-2">
+      </div>
+    </form>
+    <div wire:loading wire:target="searchByName">
+    	Loading...
+    </div>
+   @if (count($contacts) > 0)
+   	  <div class="list-group mt-2">
    		@foreach ($contacts as $contact)
    			<div class="list-group-item list-group-item-action">
    				<div class="d-flex w-100 justify-content-between">
@@ -18,5 +29,6 @@
    				<small class="text-muted">{{ $contact['phone'] }}</small>
    			</div>
    		@endforeach
-   </div>
+     </div>
+   @endif
 </div>
